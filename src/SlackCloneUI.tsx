52b72e5f448dcc6c0a1d92272832466ce2366266
Jeremy Boyer <jeremytboyer@gmail.com>
2026-05-15 04:52:44 +0000

    consolidated user list to dms and made avatars clickable

--- src/SlackCloneUI.tsx
+++ src/SlackCloneUI.tsx
@@ -313,7 +313,7 @@ export default function SlackCloneUI({
       {/* Chat */}
       <main className="flex-1 flex flex-col min-h-0">
         <div className="h-12 bg-white border-b flex items-center px-4 font-medium">
-          #{" "}
+          {" "}
           {activeChat.startsWith("dm_") ? "Direct Message" : `# ${activeChat}`}
         </div>
 
@@ -325,14 +325,35 @@ export default function SlackCloneUI({
                 msg.uid === uid ? "justify-end" : "justify-start"
               }`}
             >
-              {msg.uid === uid && (
-                <img
-                  src={msg.avatar}
-                  alt={msg.sender}
-                  className="w-10 h-10 rounded-full border bg-white"
-                />
+              {/* OTHER USER AVATAR */}
+              {msg.uid !== uid && (
+                <button
+                  onClick={() => {
+                    const dmRoom = `dm_${getDmRoom(uid, msg.uid)}`;
+
+                    setActiveChat(dmRoom);
+
+                    setUnreadCounts((prev) => ({
+                      ...prev,
+                      [dmRoom]: 0,
+                    }));
+
+                    setRecentDms((prev) => [
+                      dmRoom,
+                      ...prev.filter((room) => room !== dmRoom),
+                    ]);
+                  }}
+                  className="shrink-0"
+                >
+                  <img
+                    src={msg.avatar}
+                    alt={msg.sender}
+                    className="w-10 h-10 rounded-full border bg-white hover:ring-2 hover:ring-blue-400"
+                  />
+                </button>
               )}
 
+              {/* MESSAGE */}
               <div
                 className={`max-w-md px-3 py-2 rounded-lg text-sm ${
                   msg.uid === uid ? "bg-blue-500 text-white" : "bg-white border"
@@ -353,26 +374,13 @@ export default function SlackCloneUI({
                 <div>{msg.text}</div>
               </div>
 
-              {msg.uid !== uid && (
-                <button
-                  onClick={() => {
-                    const dmRoom = `dm_${getDmRoom(uid, msg.uid)}`;
-                    setActiveChat(dmRoom);
-
-                    setUnreadCounts((prev) => ({
-                      ...prev,
-                      [dmRoom]: 0,
-                    }));
-                  }}
-                  className="shrink-0"
-                  title={`Message ${msg.sender}`}
-                >
-                  <img
-                    src={msg.avatar}
-                    alt={msg.sender}
-                    className="w-10 h-10 rounded-full border bg-white hover:ring-2 hover:ring-blue-400"
-                  />
-                </button>
+              {/* YOUR AVATAR */}
+              {msg.uid === uid && (
+                <img
+                  src={msg.avatar}
+                  alt={msg.sender}
+                  className="w-10 h-10 rounded-full border bg-white"
+                />
               )}
             </div>
           ))}
